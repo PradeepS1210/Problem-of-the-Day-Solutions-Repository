81aa9a3c935e7755150db0d9c473aed5ee4ceca3
PradeepS1210 <sooryavansheepradeep2005@gmail.com>
2024-09-04 08:20:55 +0530

    Today POTD Solution

---
+++ GeeksforGeeks/2024 POTD Solutions/September/04-09-2024.cpp
@@ -0,0 +1,14 @@
+/*
+Asked in Accolite, Amazon
+Problem of the Day: Count ways to N'th Stair(Order does not matter)
+Problem Description: https://medium.com/@pradeepsooryavanshee1210/count-ways-to-nth-stair-order-does-not-matter-geeksforgeeks-solution-a8cc050bd26c
+*/
+
+class Solution
+{
+public:
+    int nthStair(int n)
+    {
+        return (n / 2) + 1;
+    }
+};
