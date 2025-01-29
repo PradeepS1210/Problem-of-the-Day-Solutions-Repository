ad17de42bf390c262d8f4b66cd7a55187e3e165d
Pradeep Suryavanshi <150350408+PradeepS1210@users.noreply.github.com>
2025-01-29 12:04:13 +0530

    Today POTD Solution

---
+++ GeeksforGeeks/2025 POTD Solutions/January/29-01-2025.cpp
@@ -0,0 +1,11 @@
+/*
+Problem of the Day: Implement Pow
+Problem Description: https://www.geeksforgeeks.org/problems/powx-n/1
+*/
+
+class Solution {
+public:
+    double power(double b, int e) {
+        return pow(b, e);
+    }
+};
