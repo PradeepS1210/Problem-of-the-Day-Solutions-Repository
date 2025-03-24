216dcec96d77dfbecf17b83811bcfab2ca13b55a
Pradeep Suryavanshi <150350408+PradeepS1210@users.noreply.github.com>
2025-03-24 06:43:14 +0530

    Today POTD Solution

---
+++ GeeksforGeeks/2025 POTD Solutions/March/24-03-2025.cpp
@@ -0,0 +1,24 @@
+/*
+Problem of the Day: Matrix Chain Multiplication
+Problem Description: https://www.geeksforgeeks.org/problems/matrix-chain-multiplication0303/1
+*/
+
+class Solution {
+public:
+    int matrixMultiplication(vector<int> &arr) {
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        for (int L = 2; L < n; L++) {
+            for (int i = 1; i < n - L + 1; i++) {
+                int j = i + L - 1;
+                dp[i][j] = INT_MAX;
+                for (int k = i; k < j; k++) {
+                    int cost = dp[i][k] + dp[k + 1][j] + arr[i - 1] * arr[k] * arr[j];
+                    dp[i][j] = min(dp[i][j], cost);
+                }
+            }
+        }
+        return dp[1][n - 1];
+    }
+};
